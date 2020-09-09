342990e54c24a164eb4bbe77c751fdb4b6dcad9b
Atef OUERGHI <atef.ouerghi@spa-rac.org>
2020-09-09 16:50:58 +0100

    09/09/2020

--- app/src/Controller/CatalogueAdminController.php
+++ app/src/Controller/CatalogueAdminController.php
@@ -31,7 +31,7 @@ final class CatalogueAdminController extends CRUDController
 
         $session = $request->getSession();
         $fp = fopen($_SERVER['DOCUMENT_ROOT'] . "/catalogue-import-" . date('d-m-y-H_i') . ".txt", "wb");
-        $url = '/../public/catalogue-import-' . date('d-m-y-H_i') . '.txt';
+        $url = '/catalogue-import-' . date('d-m-y-H_i') . '.txt';
         //dump($url);die;
         $request->request->set('_sonata_admin', 'admin.template');
         if (isset($_POST["submit"])) {
@@ -56,6 +56,8 @@ final class CatalogueAdminController extends CRUDController
                 $reader = new XlsxReader();
                 $spreadsheet = $reader->load($tmp_name);
                 $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+                //dump($sheetData);die;
+
 
                 $worksheet = $spreadsheet->getActiveSheet();
                 $highestRow = $worksheet->getHighestRow(); // e.g. 10
@@ -65,33 +67,61 @@ final class CatalogueAdminController extends CRUDController
                 fwrite($fp, "Highest Column : " . $highestColumn . "\n");
                 $em = $this->getDoctrine()->getManager()->getRepository(Catalogue::class);
 
-                $highestColumnIndex = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($highestColumn);
 
-                for ($row = 0; $row <= $highestRow; $row++) {
-                    for ($col = 0; $col <= $highestColumnIndex; $col++) {
-                        $value = $worksheet->getCellByColumnAndRow($col, $row)->getValue();
-                        //array_push($res,$value);
-                        $Species_catalogues = $em->findOneBy(['Species' => $value]);
+                $list = [];
+                //problem is here, seek to row 200 is nothing working
+                $it = $worksheet->getRowIterator(1);
+                //it still start at row 1
+                foreach ($it as $row) {
+                    $cellIt = $row->getCellIterator();
+                    $r = [];
+                    foreach ($cellIt as $cell) {
+                        $r[] = $cell->getFormattedValue();
                     }
-                    if ($Species_catalogues) {
-                        if ($Species_catalogues->getSpecies() == $value) {
-                            //dump($row, $Species_catalogues->getSpecies() , $value);die;
-                            //$request->getSession()->getFlashBag()->add('sucess',   $row.'-'.$Species_catalogues . '---to be Added' . '<br>');
-                            fwrite($fp, $row . '-' . $value . '-Exist already' . "\n");
-                        } else {
-                            //dump($Species_catalogues->getId());die;
-                            fwrite($fp, $row . '-' . $value . '-to be Added' . "\n");
-                            //$request->getSession()->getFlashBag()->add('error',   $row.'-'.$Species_catalogues . '---to be Added' . '<br>');
-                        }
+                    $list[] = $r;
+                }
+                //dump($list);die;
+                foreach ($list as $x) {
+                    $v = $x[0];
+                    fwrite($fp, $v . "\n");
+                    //dump($v);die;
+                    $s = $em->findOneBy(['Species' => $x[0]]);
+                    if (!$s) {
+                        dump(!$s);
+                        die;
+                        $request->getSession()->getFlashBag()->add('error', $v . '-exiting' . '<br>');
+                        //    fwrite($fp, $v.'-'.$s. "\n");
 
+                    } else {
+                        $request->getSession()->getFlashBag()->add('success', $v . '-to be added' . '<br>');
                     }
-                    //fwrite($fp, $row.'-'.$value . "\n");
+
+                    //
                 }
 
+                $highestColumnIndex = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($highestColumn);
+                for ($row = 0; $row <= $highestRow; $row++) {
+                    for ($col = 1; $col <= $highestColumnIndex; $col++) {
+                        //dump($worksheet->getCellByColumnAndRow($col, $row)->getValue());die;
+                        if ($em->findOneBy(['Species' => $worksheet->getCellByColumnAndRow($col, $row)->getValue()])) {
+                            //dump($worksheet->getCellByColumnAndRow($col, $row)->getValue());die;
+                            //dump($em->findOneBy(['Species' => $worksheet->getCellByColumnAndRow($col, $row)->getValue()]));die;
+                            if ($em->findOneBy(['Species' => $worksheet->getCellByColumnAndRow($col, $row)->getValue()])->getSpecies() == $worksheet->getCellByColumnAndRow($col, $row)->getValue()) {
+                                //dump($worksheet->getCellByColumnAndRow($col, $row)->getValue());die;
+                                //fwrite($fp, $row .'- 1' . "\n");
+                            } else {
+                                //dump($Species_catalogues->getId());die;
+
+
+                                //fwrite($fp, $row.'- 2' . "\n");
+                                //fwrite($fp, $row . '-' . $value . '-to be Added' . "\n");
+                                //$request->getSession()->getFlashBag()->add('error',   $row.'-'.$Species_catalogues . '---to be Added' . '<br>');
+                            }
+                        }
+                    }
+                }
 
                 fclose($fp);
-
-
                 $request->getSession()->getFlashBag()->add('success', 'File is valid, and was successfully processed.!' . '<br>' . "<a href=" . $url . ">Log Link</a>");
                 return $this->redirect($this->generateUrl('Catalogue_list'));
             } else {
